a46b96c09c4f502bdaf6334dba8150a7c83704cf
fabricio jacob <fabricio.jacob@outlook.com>
2022-10-28 08:19:56 -0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spring-dynamodb-pagination
+- Para saber mais sobre overloading de index no dynamodb https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/bp-gsi-overloading.html
--- src/test/java/com/github/fabriciolfj/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/com/github/fabriciolfj/accountservice/AccountServiceApplicationTests.java
@@ -3,11 +3,15 @@ package com.github.fabriciolfj.accountservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AccountServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		var test = "test" + 1;
+
+		System.out.println(test);
+
 	}
 
 }
